9407251df23fda80d110ed182469e22592bc3bc8
vskorepa-ntb <vskorepa@skori.cz>
2021-09-19 22:56:12 +0200

    dev-stack-changes vol.2

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,17 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import useTranslation from 'next-translate/useTranslation'
+import { useRouter } from 'next/router'
+import { Container, Row, Col, Link as NextUiLink } from '@nextui-org/react'
+import  Link  from 'next/link'
+
 
 const Home: NextPage = () => {
+  const router = useRouter()
+  const {t,lang} = useTranslation('common')
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,53 +22,25 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1>{t("greeting")}</h1>
       </main>
 
       <footer className={styles.footer}>
+        <ul>
+          {router.locales?.map((locale)=>(
+            <li key={locale}>
+               <Link href={router.asPath} locale={locale}> 
+               <NextUiLink block color="success">{locale}</NextUiLink>
+               </Link>
+            </li>       
+          ))}
+        </ul>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          {t("powered")}{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
