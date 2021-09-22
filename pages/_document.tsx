58e7161b6f565d27fbd53b13519ed8eb734e7708
vskorepa_PC <vskorepa@skori.cz>
2021-09-22 21:52:50 +0200

    formating changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,33 +1,40 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
-import { CssBaseline } from "@nextui-org/react";
+import React from 'react'
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+} from 'next/document'
+import { CssBaseline } from '@nextui-org/react'
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx : DocumentContext) {
-		const initialProps = await Document.getInitialProps(ctx);
-		const styles = CssBaseline.flush();
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+        const styles = CssBaseline.flush()
 
-		return {
-			...initialProps,
-			styles: (
-				<>
-					{initialProps.styles}
-					{styles}
-				</>
-			),
-		};
-	}
+        return {
+            ...initialProps,
+            styles: (
+                <>
+                    {initialProps.styles}
+                    {styles}
+                </>
+            ),
+        }
+    }
 
-	render() {
-		return (
-			<Html>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+    render() {
+        return (
+            <Html>
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
 
-export default MyDocument;
+export default MyDocument
--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,57 +1,47 @@
-import {gql, ApolloServer }from "apollo-server-micro";
+import { gql, ApolloServer } from "apollo-server-micro";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const typeDefs = gql`
+  type Ticket {
+    id: String
+    isAlive: Boolean
+    createdAt: String
+    updatedAt: String
+    operatorId: String
+    userId: String
+    ticketProblemId: String
+  }
 
-    type Ticket {
-        id: String 
-        isAlive: Boolean
-        createdAt: String
-        updatedAt: String
-        operatorId: String
-        userId: String
-        ticketProblemId: String
-    }
+  type User {
+    userId: String
+    userEmail: String
+    userName: String
+    userSurname: String
+  }
+  type Operator {
+    operatorId: String
+    operatorEmail: String
+    operatorName: String
+    operatorSurname: String
+  }
 
-    type User {
-        userId:      String  
-        userEmail:   String   
-        userName:    String
-        userSurname: String
-    }
-    type Operator {
-        operatorId:      String  
-        operatorEmail:   String   
-        operatorName:    String
-        operatorSurname: String
-    }
-
-    type Query {
-        tickets: [Ticket]
-    }
-
+  type Query {
+    tickets: [Ticket]
+  }
 `;
 
 const resolvers = {
-
-	Query: {
-		tickets: (_parent, _args, _context) => {
-			return prisma.ticket.findMany();
-		},
-	}, 
-    
+  Query: {
+    tickets: (_parent, _args, _context) => {
+      return prisma.ticket.findMany();
+    },
+  },
 };
 
-const apolloServer =  new ApolloServer({typeDefs, resolvers });
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
+export const config = { api: { bodyParser: false } };
 
-export const config = {api: { bodyParser: false,},};
-
-
-export default apolloServer.createHandler({path: "api/graphql"});
-
-
-
-
+export default apolloServer.createHandler({ path: "api/graphql" });
