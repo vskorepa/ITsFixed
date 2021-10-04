1a0fe8cbb7b25caf6cbb83d9f7b2428af53da37e
vskorepa-ntb <vskorepa@skori.cz>
2021-10-04 21:19:06 +0200

    working signIn

--- components/login/login.tsx
+++ components/login/login.tsx
@@ -1,9 +1,16 @@
 import { Container } from "@nextui-org/react";
 import React from "react";
 import SignUp from "./signUp";
+import { Text, Row } from "@nextui-org/react";
 const Login = () => {
     return (
         <Container fluid wrap="wrap" justify="center">
+            <Row justify="center">
+                <Text color="success" h1>
+                    SignIn Page
+                </Text>
+            </Row>
+
             <SignUp />
         </Container>
     );
--- components/login/signUp.tsx
+++ components/login/signUp.tsx
@@ -1,4 +1,11 @@
-import { Container, Row, Input } from "@nextui-org/react";
+import {
+    Container,
+    Row,
+    Input,
+    Loading,
+    Button,
+    Spacer,
+} from "@nextui-org/react";
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { SingUpValues } from "../../types/formtypes";
@@ -7,16 +14,19 @@ import { useState } from "react";
 
 const SignUp = () => {
     const { register, handleSubmit } = useForm<SingUpValues>();
-    const onSubmint: SubmitHandler<SingUpValues> = (data) =>
+    const onSubmint: SubmitHandler<SingUpValues> = (data) => {
         handleSignIn(data.email, data.password);
+        console.log(data);
+    };
     const [loading, setLoading] = useState(false);
     const handleSignIn = async (email: string, password: string) => {
         try {
             setLoading(true);
-            const { error } = await supabase.auth.signUp({
-                email: "example@email.com",
-                password: "example-password",
+            const { user, session, error } = await supabase.auth.signUp({
+                email: email,
+                password: password,
             });
+            console.log(user);
         } catch (error) {
             // @ts-ignore
             alert(error.error_description || error.message);
@@ -25,52 +35,60 @@ const SignUp = () => {
         }
     };
 
-    // const { user, session, error } = await supabase.auth.signUp({
-    //     email: "example@email.com",
-    //     password: "example-password",
-    // });
-
-    return (
-        <Container justify="center" wrap="wrap" fluid>
-            <form onSubmit={handleSubmit(onSubmint)}>
-                <Row justify="center">
-                    <Input
-                        size="xlarge"
-                        rounded
-                        bordered
-                        color="primary"
-                        label="Email"
-                        placeholder="example@email.com"
-                        {...register("email")}
-                    ></Input>
-                </Row>
-                <Row justify="center">
-                    <Input
-                        size="xlarge"
-                        rounded
-                        bordered
-                        color="primary"
-                        label="Password"
-                        type="password"
-                        placeholder="password"
-                        {...register("password")}
-                    ></Input>
-                </Row>
-                <Row justify="center">
-                    <Input
-                        size="xlarge"
-                        rounded
-                        bordered
-                        color="primary"
-                        label="Confirm password"
-                        type="password"
-                        placeholder="Confirm password"
-                        {...register("confirmpassword")}
-                    ></Input>
-                </Row>
-            </form>
-        </Container>
-    );
+    if (loading == true)
+        return (
+            <Container>
+                <Loading size={80} />
+            </Container>
+        );
+    else
+        return (
+            <Container justify="center" wrap="wrap" fluid>
+                <form onSubmit={handleSubmit(onSubmint)}>
+                    <Row justify="center">
+                        <Input
+                            size="xlarge"
+                            rounded
+                            bordered
+                            color="primary"
+                            label="Email"
+                            placeholder="example@email.com"
+                            {...register("email")}
+                        ></Input>
+                    </Row>
+                    <Row justify="center">
+                        <Input
+                            size="xlarge"
+                            rounded
+                            bordered
+                            color="primary"
+                            label="Password"
+                            type="password"
+                            placeholder="password"
+                            {...register("password")}
+                        ></Input>
+                    </Row>
+                    <Row justify="center">
+                        <Input
+                            size="xlarge"
+                            rounded
+                            bordered
+                            color="primary"
+                            label="Confirm password"
+                            type="password"
+                            placeholder="Confirm password"
+                            {...register("confirmpassword")}
+                        ></Input>
+                    </Row>
+                    <Spacer />
+                    <Row justify="center">
+                        <Button size="large" color="primary" type="submit">
+                            Odeslat
+                        </Button>
+                    </Row>
+                </form>
+            </Container>
+        );
 };
 
 export default SignUp;
